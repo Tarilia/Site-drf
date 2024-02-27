09c818e0bfd8724e55570d2419f2b88fd99f6b76
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-02-27 15:42:41 +0300

    add migrate

---
+++ site_drf/drf/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Sitedrf, Category
+
+
+admin.site.register(Sitedrf)
+admin.site.register(Category)
---
+++ site_drf/drf/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Sitedrf(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Статус")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True,
+                            verbose_name="Категории")
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+
+    def __str__(self):
+        return self.name
