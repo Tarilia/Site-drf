83f8f1babdf99d3126f94de93a4771ac1ceb3d68
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-03-02 13:18:53 +0300

    add ModelViewSet

--- site_drf/drf/views.py
+++ site_drf/drf/views.py
@@ -1,22 +1,27 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from site_drf.drf.models import Sitedrf
 from site_drf.drf.serializers import DogSerializer
 
 
-class DogAPIList(generics.ListCreateAPIView):
+class DogViewSet(viewsets.ModelViewSet):
     queryset = Sitedrf.objects.all()
     serializer_class = DogSerializer
 
 
-class DogAPIUpdate(generics.UpdateAPIView):
-    queryset = Sitedrf.objects.all()
-    serializer_class = DogSerializer
+# class DogAPIList(generics.ListCreateAPIView):
+#    queryset = Sitedrf.objects.all()
+#    serializer_class = DogSerializer
 
 
-class DogAPIDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Sitedrf.objects.all()
-    serializer_class = DogSerializer
+#class DogAPIUpdate(generics.UpdateAPIView):
+#    queryset = Sitedrf.objects.all()
+#    serializer_class = DogSerializer
+
+
+#class DogAPIDetail(generics.RetrieveUpdateDestroyAPIView):
+#    queryset = Sitedrf.objects.all()
+#    serializer_class = DogSerializer
 
 
 # class DogAPIView(generics.ListAPIView):
--- site_drf/urls.py
+++ site_drf/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from site_drf.drf.views import (DogAPIList, DogAPIUpdate,
-                                DogAPIDetail)
+                                DogAPIDetail, DogViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/doglist/', DogAPIList.as_view()),
-    path('api/doglist/<int:pk>/', DogAPIUpdate.as_view()),
-    path('api/dogdetail/<int:pk>/', DogAPIDetail.as_view()),
+    path('api/doglist/', DogViewSet.as_view({'get': 'list'})),
+    path('api/doglist/<int:pk>/', DogViewSet.as_view({'put': 'update'})),
+#     path('api/dogdetail/<int:pk>/', DogAPIDetail.as_view()),
 #     path('api/doglist/', DogAPIView.as_view()),
 ]
