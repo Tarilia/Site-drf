885bd69fa7a8a2f0e84f9d65acb1b3b31ead7c35
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-03-02 13:29:36 +0300

    add routers

--- site_drf/urls.py
+++ site_drf/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from site_drf.drf.views import (DogAPIList, DogAPIUpdate,
-                                DogAPIDetail, DogViewSet)
+from site_drf.drf.views import DogViewSet
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'dog', DogViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/doglist/', DogViewSet.as_view({'get': 'list'})),
-    path('api/doglist/<int:pk>/', DogViewSet.as_view({'put': 'update'})),
+    path('api/', include(router.urls))
+#     path('api/doglist/', DogViewSet.as_view({'get': 'list'})),
+#     path('api/doglist/<int:pk>/', DogViewSet.as_view({'put': 'update'})),
 #     path('api/dogdetail/<int:pk>/', DogAPIDetail.as_view()),
 #     path('api/doglist/', DogAPIView.as_view()),
 ]
