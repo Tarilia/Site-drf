ef5c698a4d0cb55efb07dea14d2dafdc0c36cf78
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-03-03 12:41:15 +0300

    add IsAuthenticatedOrReadOnly

--- site_drf/drf/views.py
+++ site_drf/drf/views.py
@@ -1,41 +1,41 @@
-from rest_framework import generics, viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from site_drf.drf.models import Sitedrf, Category
+from site_drf.drf.models import Sitedrf
 from site_drf.drf.serializers import DogSerializer
 
 
-class DogViewSet(viewsets.ModelViewSet):
-#     queryset = Sitedrf.objects.all()
+class DogAPIList(generics.ListCreateAPIView):
+    queryset = Sitedrf.objects.all()
+    serializer_class = DogSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+
+class DogAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Sitedrf.objects.all()
     serializer_class = DogSerializer
 
-    def get_queryset(self):
-        pk = self.kwargs.get('pk')
-        if not pk:
-            return Sitedrf.objects.all()[:3]
 
-        return Sitedrf.objects.filter(pk=pk)
-
-    @action(methods=['get'], detail=False)
-    def category(self, request):
-        cat = Category.objects.all()
-        return Response({'cat': [c.name for c in cat]})
+class DogAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Sitedrf.objects.all()
+    serializer_class = DogSerializer
 
 
-# class DogAPIList(generics.ListCreateAPIView):
-#    queryset = Sitedrf.objects.all()
+# class DogViewSet(viewsets.ModelViewSet):
+#     queryset = Sitedrf.objects.all()
 #    serializer_class = DogSerializer
 
+#    def get_queryset(self):
+#        pk = self.kwargs.get('pk')
+#        if not pk:
+#            return Sitedrf.objects.all()[:3]
 
-#class DogAPIUpdate(generics.UpdateAPIView):
-#    queryset = Sitedrf.objects.all()
-#    serializer_class = DogSerializer
+#        return Sitedrf.objects.filter(pk=pk)
 
-
-#class DogAPIDetail(generics.RetrieveUpdateDestroyAPIView):
-#    queryset = Sitedrf.objects.all()
-#    serializer_class = DogSerializer
+#    @action(methods=['get'], detail=False)
+#    def category(self, request):
+#        cat = Category.objects.all()
+#        return Response({'cat': [c.name for c in cat]})
 
 
 # class DogAPIView(generics.ListAPIView):
--- site_drf/urls.py
+++ site_drf/urls.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from site_drf.drf.views import DogViewSet
-from rest_framework import routers
+from site_drf.drf.views import DogAPIList, DogAPIUpdate, DogAPIDestroy
+# from rest_framework import routers
 
 
-router = routers.DefaultRouter()
-router.register(r'dog', DogViewSet, basename='dog')
+# router = routers.DefaultRouter()
+# router.register(r'dog', DogViewSet, basename='dog')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/dog/', DogAPIList.as_view()),
+    path('api/dog/<int:pk>/', DogAPIUpdate.as_view()),
+    path('api/dogdelete/<int:pk>/', DogAPIDestroy.as_view()),
+#     path('api/', include(router.urls))
 #     path('api/doglist/', DogViewSet.as_view({'get': 'list'})),
 #     path('api/doglist/<int:pk>/', DogViewSet.as_view({'put': 'update'})),
 #     path('api/dogdetail/<int:pk>/', DogAPIDetail.as_view()),
